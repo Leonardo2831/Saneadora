04ae876f7ffd809ab064916828c3be7f9da9c5e3
Leonardo2831 <leoferraz1818@gmail.com>
2026-01-13 11:31:58 -0300

    removed files

--- public/js/typescript/generate-text/generateNumbers.js
+++
@@ -1,7 +0,0 @@
-import arrayContentRow from "./getContentRow.js";
-export default function generateNumbers(tableNumbers) {
-    var numbersText = arrayContentRow(tableNumbers);
-    return numbersText.map(function (numberText) {
-        return "<li class=\"text-base font-normal text-gray-700\">\n                    <b class=\"text-gray-800 font-bold\">ATO:</b> \n                        ".concat(numberText[0], " |\n                    <b class=\"text-gray-800 font-bold\">Tipo:</b> \n                        ").concat(numberText[3], " |\n                    <b class=\"text-gray-800 font-bold\">Numera\u00E7\u00E3o:</b>\n                        ").concat(numberText[1], " |\n                    <b class=\"text-gray-800 font-bold\">CPF:</b> \n                        ").concat(numberText[2] == "" ? "Não consta" : numberText[2], "\n                </li>");
-    });
-}
--- public/js/typescript/inputs/initValueInputs.js
+++
@@ -1,8 +0,0 @@
-export default function initValueInputs() {
-    var inputs = document.querySelectorAll('input[aria-label]');
-    inputs.forEach(function (input) {
-        if (input instanceof HTMLInputElement) {
-            input.value = input.getAttribute("aria-label") || "";
-        }
-    });
-}
